gradient example fixed

examples/gradient/main.py
@@ -2,8 +2,8 @@
 options_kwargs = {
   'template_file_path': "template.dat",
   'energy_regex'      : r"@DF-RHF Final Energy:\s+(-\d+\.\d+)",
-  'success_regex'     : r"\*\*\* P[Ss][Ii]4 exiting successfully." ,
   'program'           : "psi4",
+  'wait_time'         : 120,
   'input_name'        : "input.dat",
   'output_name'       : "output.dat"
 }
@@ -14,12 +14,12 @@ options_obj = options.Options(**options_kwargs)
 from optavc.template import TemplateFileProcessor
 tfp = TemplateFileProcessor(open("template.dat").read(), options_obj)
 
-from optavc.gradient import Gradient
+from optavc.findifcalcs import Gradient
 import numpy as np
-gradient_obj = Gradient(tfp.molecule, tfp.input_file_object, options_obj, path="GRAD")
-gradient_obj.compute_gradient()
-grad = gradient_obj.get_gradient()
-enrg = gradient_obj.get_reference_energy()
+grad_obj = Gradient(tfp.molecule, tfp.input_file_object, options_obj, path="GRAD")
+grad_obj.compute_result()
+grad = grad_obj.reap()
+enrg = grad_obj.get_reference_energy()
 print(grad)
 print(np.array(grad))
 print(enrg)