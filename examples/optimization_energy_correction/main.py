1541df00f87a8e5e0ad52565ae8d7323d9680d27
Erica Caiyi Mitchell <ecm23353@ss-sub3.gacrc.uga.edu>
2022-06-06 17:21:34 -0400

    Fix opt_energy_corr and opt_restart

--- examples/optimization_energy_correction/main.py
+++ examples/optimization_energy_correction/main.py
@@ -3,7 +3,6 @@ options_kwargs = {
   'template_file_path': "template.dat",
   'energy_regex'      : r"@DF-RHF Final Energy:\s+(-\d+\.\d+)",
   'correction_regexes': [r"\n\s+Correlation Energy\s+=\s+(-\d+\.\d+)"],
-  'success_regex'     : r"\*\*\* P[Ss][Ii]4 exiting successfully." ,
   'program'           : "psi4",
   'input_name'        : "input.dat",
   'output_name'       : "output.dat",
