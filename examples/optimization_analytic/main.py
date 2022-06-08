ece69dbbeae44b5c86d0af2d7dc6b2cf35e711ca
Erica Mitchell <ecm23353@vlogin2.ccqc.uga.edu>
2022-06-08 14:39:47 -0400

    Fix regex in opt_analytic example

--- examples/optimization_analytic/main.py
+++ examples/optimization_analytic/main.py
@@ -2,7 +2,7 @@ import optavc
 
 options_kwargs = {
   'template_file_path': "template.dat",
-  'deriv_regex'      : r"\s*-Total\sGradient:\n\s*Atom\s*X\s*Y\s*Z\n(\s*-*)*\n",
+  'deriv_regex'      : r"\s*-Total\s[Gg]radient:\n\s*Atom\s*X\s*Y\s*Z\n(\s*-*)*\n",
   'energy_regex'      : r"\s*@DF-RHF Final Energy:\s+(-\d+\.\d+)",
   'program'           : "psi4",
   'input_name'        : "input.dat",
