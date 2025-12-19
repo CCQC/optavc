f0cce2232c747bae3feef57178848b97d459d320
Connor Briggs <cgbriggs99@gmail.com>
2025-12-19 15:18:59 -0500

    Update sapelo_programs.py

--- submitscripts/slurm/sapelo_programs.py
+++ submitscripts/slurm/sapelo_programs.py
@@ -35,7 +35,8 @@ export SCRATCH_DIR=/scratch/$USER/tmp/$SLURM_JOB_ID
 mkdir -p $SCRATCH_DIR
 export APPTAINER_BIND="$SLURM_SUBMIT_DIR,$SCRATCH_DIR"  # This binds the directory into the container so that output can be written.
 
-singularity run /work/jttlab/containers/molpro-2024.1.1-mpi-gapr.sif -n $NSLOTS input.dat --output $SLURM_SUBMIT_DIR/output.dat --nouse-logfile --directory $SCRATCH_DIR
+singularity run /work/jttlab/containers/molpro-2024.1.1-mpi-gapr.sif -n $NSLOTS input.dat --output $SLURM_SUBMIT_DIR/output.dat --nouse-logfile --directory $SCRATCH_DIR \
+-m $(($SLURM_MEM_PER_NODE/10/$NSLOTS))M # Divide by 10 so that it leaves room for extra variables. Word size is normally 8.
 rm $SCRATCH_DIR -r
 
 """
@@ -49,8 +50,8 @@ export SCRATCH_DIR=/lscratch/$USER/tmp/$SLURM_JOB_ID
 mkdir -p $SCRATCH_DIR
 export APPTAINER_BIND="$SLURM_SUBMIT_DIR,$SCRATCH_DIR"  # This binds the directory into the container so that output can be written.
 
-
-singularity run /work/jttlab/containers/molpro-2024.1.1-mpi-gapr.sif -n $NSLOTS input.dat --output $SLURM_SUBMIT_DIR/output.dat --nouse-logfile --directory $SCRATCH_DIR
+singularity run /work/jttlab/containers/molpro-2024.1.1-mpi-gapr.sif -n $NSLOTS input.dat --output $SLURM_SUBMIT_DIR/output.dat --nouse-logfile --directory $SCRATCH_DIR \
+-m $(($SLURM_MEM_PER_NODE/10/$NSLOTS))M # Divide by 10 so that it leaves room for extra variables. Word size is normally 8.
 
 rm $SCRATCH_DIR -r
 
